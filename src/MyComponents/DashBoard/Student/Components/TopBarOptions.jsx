4d505a8d00ea4b3efb90acc88de488c4c1ff1959
mattscepter <chaudharyut25@gmail.com>
2021-10-02 14:12:45 +0530

    navoptions

---
+++ src/MyComponents/DashBoard/Student/Components/TopBarOptions.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+import { signout } from '../../../../helpers/auth';
+
+function TopBarOptions({ options }) {
+  const history = useHistory();
+  return (
+    <div
+      className={`absolute right-28 top-20 ${
+        options
+          ? 'transition-all duration-200 opacity-100 inline-block'
+          : 'transition-all duration-200 opacity-0 invisible'
+      }`}
+    >
+      <div className="flex items-center p-4 hover:bg-red-1 hover:text-white cursor-pointer w-64 h-12 mt-2 filter drop-shadow-sm rounded-xl bg-white">
+        My Purchases
+      </div>
+      <div className="flex items-center p-4 hover:bg-red-1 hover:text-white cursor-pointer w-64 h-12 mt-2 filter drop-shadow-sm rounded-xl bg-white">
+        My Transcripts
+      </div>
+      <div className="flex items-center p-4 hover:bg-red-1 hover:text-white cursor-pointer w-64 h-12 mt-2 filter drop-shadow-sm rounded-xl bg-white">
+        Upload Documents
+      </div>
+      <div className="flex items-center p-4 hover:bg-red-1 hover:text-white cursor-pointer w-64 h-12 mt-2 filter drop-shadow-sm rounded-xl bg-white">
+        Change Password
+      </div>
+      <div
+        onClick={() => {
+          history.push('/');
+          signout();
+        }}
+        className="flex items-center p-4 hover:bg-red-1 hover:text-white cursor-pointer w-64 h-12 mt-2 filter drop-shadow-sm rounded-xl bg-white"
+      >
+        Logout
+      </div>
+    </div>
+  );
+}
+
+export default TopBarOptions;
--- src/MyComponents/DashBoard/Student/Home.jsx
+++ src/MyComponents/DashBoard/Student/Home.jsx
@@ -1,16 +1,7 @@
-import React from "react";
-import { SideNav } from "./Components/SideNav";
-import { TopBar } from "./Components/TopBar";
+import React from 'react';
+import { SideNav } from './Components/SideNav';
+import { TopBar } from './Components/TopBar';
 
 export default function Home() {
-  return (
-    <div className="w-full flex flew-col md:flex-row">
-      <div className="w-2/12 md:w-72 xl:w-96 bg-red-1 shadow-button-inner">
-        <SideNav />
-      </div>
-      <div className="w-full bg-client">
-        <TopBar />
-      </div>
-    </div>
-  );
+  return <div className="w-full flex flew-col md:flex-row"></div>;
 }
