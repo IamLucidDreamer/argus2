cd60d2b937e5758601d4e0bba361ba0b7dfb6d72
mattscepter <chaudharyut25@gmail.com>
2021-10-04 17:07:35 +0530

    added calendar

--- src/MyComponents/DashBoard/Student/Calendar.jsx
+++ src/MyComponents/DashBoard/Student/Calendar.jsx
@@ -1,11 +1,39 @@
-import React from 'react'
+import React from 'react';
+import {
+  Calendar as StudentCalendar,
+  momentLocalizer,
+} from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './calendar.css';
 
 const Calendar = () => {
+  const localizer = momentLocalizer(moment);
   return (
     <div>
-      Hello World
+      <StudentCalendar
+        localizer={localizer}
+        events={[
+          {
+            id: 0,
+            title: 'Today Event 1',
+            start: new Date(new Date().setHours(new Date().getHours() - 7)),
+            end: new Date(new Date().setHours(new Date().getHours() - 4)),
+          },
+          {
+            id: 1,
+            title: 'Today Event 2',
+            start: new Date(new Date().setHours(new Date().getHours() - 3)),
+            end: new Date(new Date().setHours(new Date().getHours() + 3)),
+          },
+        ]}
+        resizable
+        defaultView="month"
+        style={{ height: '80vh' }}
+        className="p-2 lg:p-10"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Calendar;
