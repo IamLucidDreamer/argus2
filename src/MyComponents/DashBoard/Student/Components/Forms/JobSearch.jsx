b8640be346497dc3a99d7b3d3d73cacd86d706a2
Manas Shukla <shuklamanasofficial@gmail.com>
2021-10-12 23:19:45 +0530

    Width of the forms fixed

--- src/MyComponents/DashBoard/Student/Components/Forms/JobSearch.jsx
+++ src/MyComponents/DashBoard/Student/Components/Forms/JobSearch.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const JobSearch = () => {
     return (
-        <div className="w-96 lg:w-1/2 mx-auto">
+        <div className="w-full lg:w-1/2 mx-auto">
             <form className="rounded-lg bg-white mx-4 md:mx-8 my-4 p-2 md:p-4 shadow-button-shadow-2 h-96 overflow-y-scroll">
                 <h1>HelloTesting</h1>
                 <h1>HelloTesting</h1>
