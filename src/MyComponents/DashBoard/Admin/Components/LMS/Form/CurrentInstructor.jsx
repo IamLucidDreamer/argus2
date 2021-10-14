b95dc95ccebaa09e0ed0b4652ae7c11312f4622c
Manas Shukla <shuklamanasofficial@gmail.com>
2021-10-15 05:27:15 +0530

    Instructors Management

--- src/MyComponents/DashBoard/Admin/Components/LMS/Form/CurrentInstructor.jsx
+++ src/MyComponents/DashBoard/Admin/Components/LMS/Form/CurrentInstructor.jsx
@@ -1,42 +1,52 @@
 import React from 'react'
+import Profile from './../../../../../../argus website/PNG/IMG_0118.png'
 
 const CurrentInstructor = () => {
     return (
         <div>
       <div className="hidden lg:flex flex-row text-base xl:text-lg items-stretch mb-2">
-        <h1 className="text-center w-full lg:w-4/12 px-3 py-3 text-gray-2 font-bold rounded-xl border-2 bg-client mx-1">
-          Instructor ID
-        </h1>
-        <h1 className="text-center lg:w-5/12 px-3 py-3 text-gray-2 font-bold rounded-xl border-2 bg-client mx-1">
-          Instructor Name
-        </h1>
-        <h1 className="text-center lg:w-3/12 px-3 py-3 text-gray-2 font-bold rounded-xl border-2 bg-client mx-1">
-          Date Added
-        </h1>
+                <h1 className="text-center w-full lg:w-3/12 px-3 py-3 text-gray-2 font-bold rounded-xl border-2 bg-client mx-1">
+                Instructor ID
+                </h1>
+                <h1 className="text-center lg:w-4/12 px-3 py-3 text-gray-2 font-bold rounded-xl border-2 bg-client mx-1">
+                Instructor Name
+                </h1>
+                <h1 className="text-center lg:w-3/12 px-3 py-3 text-gray-2 font-bold rounded-xl border-2 bg-client mx-1">
+                Phone No.
+                </h1>
+                <h1 className="text-center lg:w-2/12 px-3 py-3 text-gray-2 font-bold rounded-xl border-2 bg-client mx-1">
+                Action
+                </h1>
       </div>
       <div className="flex flex-col lg:flex-row text-lg mb-2 rounded-xl shadow-cards lg:shadow-none">
-        <div className="flex flex-col justify-center text-center lg:w-4/12 px-3 py-3 text-gray-2 rounded-xl border-2 mx-1 my-1 lg:my-0 text-lg lg:text-sm xl:text-lg">
-          <h1 className="font-bold">123456789010</h1>
-        </div>
-        <div className="flex flex-col justify-center text-center lg:w-5/12 px-3 py-3 text-gray-2 rounded-xl border-2 mx-1 my-1 lg:my-0 text-lg lg:text-sm xl:text-lg">
-          <h1 className="">Testing test testing</h1>
-        </div>
         <div className="flex flex-col justify-center text-center lg:w-3/12 px-3 py-3 text-gray-2 rounded-xl border-2 mx-1 my-1 lg:my-0 text-lg lg:text-sm xl:text-lg">
-          <h1 className="">(yy/mm/dd)</h1>
-          <h1 className="font-bold">23:00</h1>
+          <h1 className="">123456789010</h1>
+        </div>
+        <div className="flex flex-row justify-start items-center text-center lg:w-4/12 px-3 py-3 text-gray-2 rounded-xl border-2 mx-1 my-1 lg:my-0 text-lg lg:text-sm xl:text-lg">
+          <img src={Profile} alt="" className="w-16 h-16 rounded-xl mr-5"/>
+          <h1 className="font-bold">Testing test testing</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center lg:w-3/12 text-gray-2 rounded-xl mx-1 my-1 lg:my-0 border-2  text-lg lg:text-sm xl:text-lg">
+          <h1 className="">8234559823</h1>
+        </div>
+        <div className="flex flex-row justify-evenly text-center lg:w-2/12 text-gray-2 rounded-xl mx-1 my-1 lg:my-0 text-xl lg:text-sm xl:text-xl font-bold">
+          <h1 className="w-full bg-red-1 my-auto rounded-xl py-4 text-white hover:text-red-1 hover:bg-white border-2 border-red-1">DELETE</h1>
         </div>
       </div>
       <div className="block lg:hidden bg-red-1 w-full h-0.5 my-4 bg-opacity-0"></div>
       <div className="flex flex-col lg:flex-row text-lg mb-2 rounded-xl shadow-cards lg:shadow-none">
-        <div className="flex flex-col justify-center text-center lg:w-4/12 px-3 py-3 text-gray-2 rounded-xl border-2 mx-1 my-1 lg:my-0 text-lg lg:text-sm xl:text-lg">
-          <h1 className="font-bold">123456789010</h1>
-        </div>
-        <div className="flex flex-col justify-center text-center lg:w-5/12 px-3 py-3 text-gray-2 rounded-xl border-2 mx-1 my-1 lg:my-0 text-lg lg:text-sm xl:text-lg">
-          <h1 className="">Testing test testing</h1>
-        </div>
         <div className="flex flex-col justify-center text-center lg:w-3/12 px-3 py-3 text-gray-2 rounded-xl border-2 mx-1 my-1 lg:my-0 text-lg lg:text-sm xl:text-lg">
-          <h1 className="">(yy/mm/dd)</h1>
-          <h1 className="font-bold">23:00</h1>
+          <h1 className="">123456789010</h1>
+        </div>
+        <div className="flex flex-row justify-start items-center text-center lg:w-4/12 px-3 py-3 text-gray-2 rounded-xl border-2 mx-1 my-1 lg:my-0 text-lg lg:text-sm xl:text-lg">
+          <img src={Profile} alt="" className="w-16 h-16 rounded-xl mr-5"/>
+          <h1 className="font-bold">Testing test testing</h1>
+        </div>
+        <div className="flex flex-col justify-center text-center lg:w-3/12 text-gray-2 rounded-xl mx-1 my-1 lg:my-0 border-2  text-lg lg:text-sm xl:text-lg">
+          <h1 className="">8234559823</h1>
+        </div>
+        <div className="flex flex-row justify-evenly text-center lg:w-2/12 text-gray-2 rounded-xl mx-1 my-1 lg:my-0 text-xl lg:text-sm xl:text-xl font-bold">
+          <h1 className="w-full bg-red-1 my-auto rounded-xl py-4 text-white hover:text-red-1 hover:bg-white border-2 border-red-1">DELETE</h1>
         </div>
       </div>
     </div>
--- src/MyComponents/DashBoard/Admin/LmsInstructor.jsx
+++ src/MyComponents/DashBoard/Admin/LmsInstructor.jsx
@@ -19,8 +19,8 @@ const LmsInstructor = () => {
         <NavLms />
         <div className="bg-white shadow-button-shadow-2 max-w-1366 mx-3 2xl:mx-auto mt-36 md:mt-0 mb-10 md:my-16 rounded-2xl">
           <nav className="flex flex-col md:flex-row text-gray-3 text-lg items-center">
-            <button onClick={() =>setShow(1)} className={`w-full md:w-1/2 py-4 rounded-2xl font-bold -mt-28 md:-mt-8 bg-white hover:shadow-none ${show ===1 ? 'shadow-none' : "shadow-button-shadow-2"}`}>Current Instructor</button>
-            <button onClick={() =>setShow(2)} className={`w-full md:w-1/2 py-4 rounded-2xl font-bold mt-4 md:-mt-8 bg-white hover:shadow-none ${show ===2 ? 'shadow-none' : "shadow-button-shadow-2"}`}>Add New Instructor</button>
+            <button onClick={() =>setShow(1)} className={`w-full md:w-1/2 py-4 rounded-2xl font-bold -mt-28 md:-mt-8 bg-white hover:shadow-none ${show ===1 ? 'shadow-none' : "shadow-button-shadow-2"}`}>Current Instructors</button>
+            <button onClick={() =>setShow(2)} className={`w-full md:w-1/2 py-4 rounded-2xl font-bold mt-4 md:-mt-8 bg-white hover:shadow-none ${show ===2 ? 'shadow-none' : "shadow-button-shadow-2"}`}>Manage Instructors</button>
           </nav>
           <div className="p-4">
             <div className={show === 1 ? "block" : "hidden"}>
