33d4f6b06a377d8ea8276ca6961d39943b6f87ed
Manas Shukla <shuklamanasofficial@gmail.com>
2021-10-31 23:26:52 +0530

    TopBar Opening Menu changed

--- src/MyComponents/DashBoard/Student/Components/TopBarOptions.jsx
+++ src/MyComponents/DashBoard/Student/Components/TopBarOptions.jsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router';
-import profile from '../../../../argus website/PNG/IMG_0118.png';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from "react-router";
+import profile from "../../../../argus website/PNG/IMG_0118.png";
+import { Link } from "react-router-dom";
 import {
   clearStorage,
   setToken,
-} from '../../../../context/actions/authActions/setStorageAction';
+} from "../../../../context/actions/authActions/setStorageAction";
 import {
   isAuthenticated,
   setUser,
-} from '../../../../context/actions/authActions/getUserAction';
+} from "../../../../context/actions/authActions/getUserAction";
 
 function TopBarOptions({ options }) {
   const history = useHistory();
   const dispatch = useDispatch();
   return (
     <div
-      className={`z-50 w-64 sm:w-72 absolute top-20 font-medium text-gray-3 bg-bg-card shadow-button-shadow-2 rounded-2xl ${
+      className={`z-50 w-64 sm:w-72 absolute top-20 right-6 xl:right-10 2xl:right-32 font-medium text-gray-3 bg-bg-card shadow-button-shadow-2 rounded-2xl ${
         options
-          ? 'transition-all duration-300 opacity-100 inline-block'
-          : 'transition-all duration-300 opacity-0 invisible'
+          ? "transition-all duration-300 opacity-100 inline-block"
+          : "transition-all duration-300 opacity-0 invisible"
       }`}
     >
       <div className="bg-gray-3 flex flex-row items-center rounded-t-xl">
@@ -37,13 +37,13 @@ function TopBarOptions({ options }) {
       <div className="text-gray-2 text-base font-bold bg-gray-200 rounded-b-xl">
         <Link to="/dashboard/student/mypurchases">
           <h1 className="rounded-lg py-4 pl-2 hover:bg-red-1 hover:text-white hover:rounded-md hover:shadow-button-inner border-b-2 border-white">
-            <span className="mx-0.5 sm:mx-4 md:mx-1 lg:mx-0.5 xl:mx-4">➔</span>{' '}
+            <span className="mx-0.5 sm:mx-4 md:mx-1 lg:mx-0.5 xl:mx-4">➔</span>{" "}
             My Purchases
           </h1>
         </Link>
         <Link to="/dashboard/student/mytranscripts">
           <h1 className="rounded-lg py-4 pl-2 hover:bg-red-1 hover:text-white hover:rounded-md hover:shadow-button-inner border-b-2 border-white">
-            <span className="mx-0.5 sm:mx-4 md:mx-1 lg:mx-0.5 xl:mx-4">➔</span>{' '}
+            <span className="mx-0.5 sm:mx-4 md:mx-1 lg:mx-0.5 xl:mx-4">➔</span>{" "}
             My Transcripts
           </h1>
         </Link>
@@ -55,22 +55,22 @@ function TopBarOptions({ options }) {
         </Link>
         <Link to="/dashboard/student/changepassword">
           <h1 className="rounded-lg py-4 pl-2 hover:bg-red-1 hover:text-white hover:rounded-md hover:shadow-button-inner border-b-2 border-white">
-            <span className="mx-0.5 sm:mx-4 md:mx-1 lg:mx-0.5 xl:mx-4">➔</span>{' '}
+            <span className="mx-0.5 sm:mx-4 md:mx-1 lg:mx-0.5 xl:mx-4">➔</span>{" "}
             Change Password
           </h1>
         </Link>
         <Link
           onClick={() => {
             dispatch(clearStorage());
-            dispatch(isAuthenticated('false'));
+            dispatch(isAuthenticated("false"));
             dispatch(setUser({}));
             dispatch(setToken(null));
 
-            history.push('/');
+            history.push("/");
           }}
         >
           <h1 className="rounded-lg py-4 pl-2 hover:bg-red-1 hover:text-white hover:rounded-md hover:shadow-button-inner">
-            <span className="mx-0.5 sm:mx-4 md:mx-1 lg:mx-0.5 xl:mx-4">➔</span>{' '}
+            <span className="mx-0.5 sm:mx-4 md:mx-1 lg:mx-0.5 xl:mx-4">➔</span>{" "}
             Logout
           </h1>
         </Link>
