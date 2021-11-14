dc6434ca71efb08f8454e21bd9c107307ebc255d
Utkarsh Chaudhary <chaudharyut25@gmail.com>
2021-11-14 20:26:02 +0530

    Update axiosInstance.js

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,8 +1,8 @@
 const { default: axios } = require('axios');
 
 const axiosInstance = axios.create({
-  // baseURL: 'https://argus-backendzedd.herokuapp.com/api',
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://argus-backendzedd.herokuapp.com/api',
+  //baseURL: 'http://localhost:8000/api',
 });
 
 export default axiosInstance;
